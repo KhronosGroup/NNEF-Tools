63d494dfe2399d837d410591999c7452290a3567
Viktor Gyenes <viktor.gyenes@aimotive.com>
2018-10-19 11:43:29 +0200

    fixing binary reader/writer to emit a header of the right size

---
+++ fix_nnef_binary_size.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import struct
+
+
+def fix_nnef_binary(in_fn, out_fn):
+    header_size = 128
+
+    with open(in_fn, 'rb') as file:
+        file_size = os.fstat(file.fileno()).st_size
+        header = file.read(header_size)
+        excess = file.read(4)
+        data = file.read()
+
+    [magic1, magic2, major, minor] = bytearray(header[:4])
+    if magic1 != 0x4E or magic2 != 0xEF or major != 1 or minor != 0:
+        return False
+
+    data_length, = struct.unpack('i', header[4:8])
+
+    if file_size != header_size + data_length + 4:
+        return False
+
+    with open(out_fn, 'wb') as file:
+        file.write(header)
+        file.write(data)
+
+    return True
+
+
+def fix_nnef_binaries(in_path, out_path):
+    for root, dirs, files in os.walk(in_path):
+        for filename in files:
+            if not filename.startswith('.'):
+                in_fn = os.path.join(root, filename)
+                out_fn = os.path.join(out_path, os.path.relpath(in_fn, in_path))
+                if os.path.splitext(filename)[1] == '.dat':
+                    if fix_nnef_binary(in_fn, out_fn):
+                        print('Fixed file: ' + in_fn)
+                elif out_fn != in_fn:
+                    with open(in_fn, 'rb') as in_file, open(out_fn, 'wb') as out_file:
+                        out_file.write(in_file.read())
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('input path must be provided')
+        exit(-1)
+    elif len(sys.argv) > 3:
+        print('too many arguments provided')
+        exit(-1)
+
+    fix_nnef_binaries(in_path=sys.argv[1], out_path=sys.argv[2] if len(sys.argv) == 3 else sys.argv[1])
--- parser/cpp/python/nnef.py
+++ parser/cpp/python/nnef.py
@@ -14,6 +14,8 @@
 
 
 import _nnef
+import sys
+import os
 import numpy as np
 from collections import OrderedDict
 
@@ -333,18 +335,34 @@ def _rank_of(shape):
     return rank
 
 
+_is_little_endian = sys.byteorder == 'little'
+
+
+def _tofile(data, file):
+    if not _is_little_endian and data.dtype != np.uint8 and data.dtype != np.int8:
+        data = data.byteswap()
+    data.tofile(file)
+
+
+def _fromfile(file, dtype, count):
+    data = np.fromfile(file, dtype, count)
+    if not _is_little_endian and data.dtype != np.uint8 and data.dtype != np.int8:
+        data = data.byteswap()
+    return data
+
+
 def write_tensor(file, tensor, version=(1,0), quantization={}):
-    np.asarray([0x4E, 0xEF, version[0], version[1]], dtype=np.uint8).tofile(file)
+    _tofile(np.asarray([0x4E, 0xEF, version[0], version[1]], dtype=np.uint8), file)
 
     dtype, bits = _numpy_dtype_split(tensor.dtype)
-    data_length = (np.prod(tensor.shape) * bits + 7) / 8
-    np.asarray([data_length, tensor.ndim], dtype=np.uint32).tofile(file)
+    data_length = int((np.prod(tensor.shape) * bits + 7) // 8)
+    _tofile(np.asarray([data_length, tensor.ndim], dtype=np.uint32), file)
 
     if tensor.ndim > MaxTensorRank:
         raise ValueError('tensor rank exceeds maximum possible value of {}'.format(MaxTensorRank))
 
-    np.asarray(tensor.shape, dtype=np.uint32).tofile(file)
-    np.asarray([0] * (MaxTensorRank - tensor.ndim), dtype=np.uint32).tofile(file);
+    _tofile(np.asarray(tensor.shape, dtype=np.uint32), file)
+    _tofile(np.asarray([0] * (MaxTensorRank - tensor.ndim), dtype=np.uint32), file)
 
     code = quantization.get('code', QUANT_CODE_FLOAT if dtype == np.float else QUANT_CODE_INTEGER)
     if (code == QUANT_CODE_FLOAT and dtype != np.float) or \
@@ -352,7 +370,7 @@ def write_tensor(file, tensor, version=(1,0), quantization={}):
         ((code == QUANT_CODE_LINEAR or code == QUANT_CODE_LOGARITHMIC) and dtype != np.uint):
         raise ValueError('incompatible quantization code ({}) and tensor dtype ({})'.format(code, tensor.dtype))
 
-    np.asarray([bits, code], dtype=np.uint32).tofile(file)
+    _tofile(np.asarray([bits, code], dtype=np.uint32), file)
 
     params = [0] * 8
     if code == QUANT_CODE_LINEAR or code == QUANT_CODE_LOGARITHMIC:
@@ -363,37 +381,50 @@ def write_tensor(file, tensor, version=(1,0), quantization={}):
     elif code != QUANT_CODE_FLOAT:
         raise ValueError('unsupported item type code: {}'.format(code))
 
-    np.asarray(params, dtype=np.float32).tofile(file)
-    np.asarray([0] * 12, dtype=np.uint32).tofile(file)
+    _tofile(np.asarray(params, dtype=np.float32), file)
+    _tofile(np.asarray([0] * 11, dtype=np.uint32), file)
 
-    tensor.tofile(file)
+    data = np.packbits(tensor) if bits == 1 else tensor
+    _tofile(data, file)
 
 
 def read_tensor(file):
-    [magic1, magic2, major, minor] = np.fromfile(file, dtype=np.uint8, count=4)
+    [magic1, magic2, major, minor] = _fromfile(file, dtype=np.uint8, count=4)
     if magic1 != 0x4E or magic2 != 0xEF:
         raise ValueError('not a valid NNEF file')
     if major > 1 or minor > 0:
         raise ValueError('unsupported file version')
 
-    [data_length, rank] = np.fromfile(file, dtype=np.uint32, count=2)
+    [data_length, rank] = _fromfile(file, dtype=np.uint32, count=2)
+
+    header_size = 128
+    file_size = os.fstat(file.fileno()).st_size
+    if file_size != header_size + data_length:
+        raise ValueError('invalid tensor file; size does not match header info')
 
     if rank > MaxTensorRank:
         raise ValueError('tensor rank exceeds maximum possible value of {}'.format(MaxTensorRank))
 
-    shape = np.fromfile(file, dtype=np.uint32, count=MaxTensorRank)
+    shape = _fromfile(file, dtype=np.uint32, count=MaxTensorRank)
     shape = shape[:rank]
 
-    [bits, code] = np.fromfile(file, dtype=np.uint32, count=2)
-    params = np.fromfile(file, dtype=np.float32, count=8)
-    padding = np.fromfile(file, dtype=np.uint32, count=12)
+    [bits, code] = _fromfile(file, dtype=np.uint32, count=2)
+    params = _fromfile(file, dtype=np.float32, count=8)
+    padding = _fromfile(file, dtype=np.uint32, count=11)
 
     signed = params[0] != 0 if code == QUANT_CODE_INTEGER else False
 
     count = np.prod(shape)
-    data = np.fromfile(file, dtype=_numpy_dtype_make(code,bits,signed), count=count)
-    if len(data) != count:
-        raise ValueError('could not read tensor data')
+    if bits == 1:
+        byte_count = int((count + 7) // 8)
+        data = _fromfile(file, dtype=np.uint8, count=byte_count)
+        if len(data) != byte_count:
+            raise ValueError('could not read tensor data')
+        data = np.unpackbits(data).astype(np.bool)[:count]
+    else:
+        data = _fromfile(file, dtype=_numpy_dtype_make(code,bits,signed), count=count)
+        if len(data) != count:
+            raise ValueError('could not read tensor data')
 
     tensor = data.reshape(shape)
 
@@ -409,40 +440,40 @@ def read_tensor(file):
 
 
 def _write_tensor_provisional(file, tensor, version=(1,0)):
-    np.asarray([0x4E, 0xEF, version[0], version[1]], dtype=np.uint8).tofile(file)
+    _tofile(np.asarray([0x4E, 0xEF, version[0], version[1]], dtype=np.uint8), file)
 
     header_length = 4 + 4 + (tensor.ndim + 1) * 4 + 4
-    np.asarray([header_length], dtype=np.uint32).tofile(file)
+    _tofile(np.asarray([header_length], dtype=np.uint32), file)
 
-    np.asarray([tensor.ndim], dtype=np.uint32).tofile(file)
-    np.asarray(tensor.shape, dtype=np.uint32).tofile(file)
+    _tofile(np.asarray([tensor.ndim], dtype=np.uint32), file)
+    _tofile(np.asarray(tensor.shape, dtype=np.uint32), file)
 
     dtype, bits = _numpy_dtype_split(tensor.dtype)
     code = 0 if dtype == np.float else 3
-    np.asarray([code, bits], dtype=np.uint8).tofile(file)
+    _tofile(np.asarray([code, bits], dtype=np.uint8), file)
 
-    np.asarray([0], dtype=np.uint16).tofile(file)
+    _tofile(np.asarray([0], dtype=np.uint16), file)
 
-    tensor.tofile(file)
+    _tofile(tensor, file)
 
 
 def _read_tensor_provisional(file):
-    [magic1, magic2, major, minor] = np.fromfile(file, dtype=np.uint8, count=4)
+    [magic1, magic2, major, minor] = _fromfile(file, dtype=np.uint8, count=4)
     if magic1 != 0x4E or magic2 != 0xEF:
         raise ValueError('not a valid NNEF file')
     if major > 1 or minor > 0:
         raise ValueError('unsupported file version')
 
-    [header_length] = np.fromfile(file, dtype=np.uint32, count=1)
+    [header_length] = _fromfile(file, dtype=np.uint32, count=1)
 
-    [rank] = np.fromfile(file, dtype=np.uint32, count=1)
-    shape = np.fromfile(file, dtype=np.uint32, count=rank)
+    [rank] = _fromfile(file, dtype=np.uint32, count=1)
+    shape = _fromfile(file, dtype=np.uint32, count=rank)
 
-    [code, bits] = np.fromfile(file, dtype=np.uint8, count=2)
-    [qlen] = np.fromfile(file, dtype=np.uint16, count=1)
+    [code, bits] = _fromfile(file, dtype=np.uint8, count=2)
+    [qlen] = _fromfile(file, dtype=np.uint16, count=1)
 
     assert(code == 0)
     assert(bits == 32)
     assert(qlen == 0)
 
-    return np.fromfile(file, dtype=np.float32, count=np.prod(shape)).reshape(shape)
+    return _fromfile(file, dtype=np.float32, count=np.prod(shape)).reshape(shape)
