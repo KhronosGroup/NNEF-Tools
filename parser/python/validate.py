0bb2950ce7843c24408d248a20354e5ce9c8431f
Tamas Danyluk <tamas.danyluk@aimotive.com>
2019-04-10 12:44:52 +0200

    Update parser/validate.py

--- parser/python/validate.py
+++ parser/python/validate.py
@@ -6,11 +6,24 @@ if __name__ == '__main__':
 
     ap = argparse.ArgumentParser()
     ap.add_argument('path', type=str, help='path to the model to validate')
+    ap.add_argument('--stdlib', type=str, help='file name of alternate standard operation definitions '
+                                               '(defaults to all-primitive definitions)', default='')
+    ap.add_argument('--lower', type=str, help='comma separated list of operations to lower (if defined as compound)',
+                    default='')
     ap.add_argument('--shapes', action="store_true", help='perform shape validation as well')
     args = ap.parse_args()
+
+    stdlib = ''
+    if args.stdlib:
+        try:
+            with open(args.stdlib) as file:
+                stdlib = file.read()
+        except FileNotFoundError as e:
+            print('Could not open file: ' + args.stdlib)
+            exit(-1)
     
     try:
-        graph = nnef.load_graph(args.path)
+        graph = nnef.load_graph(args.path, stdlib=stdlib, lowered=args.lower.split(','))
     except nnef.Error as err:
         print('Parse error: ' + str(err))
         exit(-1)
