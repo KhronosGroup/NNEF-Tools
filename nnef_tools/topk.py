cdd0ade850525dc3840230ce25b2d50cdc7cd200
Danyluk Tamás <tamas.danyluk2@gmail.com>
2019-09-13 16:07:01 +0200

    Added stdin support to topk

--- nnef_tools/topk.py
+++ nnef_tools/topk.py
@@ -28,8 +28,10 @@ import argparse
 
 import numpy as np
 import os
+import nnef
 
 from nnef_tools.io.nnef.nnef_io import read_nnef_tensor
+from nnef_tools.core import utils
 
 
 def topk(data, axis, k):
@@ -43,7 +45,10 @@ def get_args():
         description="NNEF-Tools/topk.py: Print top k elements of a tensor along the given axis",
         formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('tensor_path', help="""The path of the tensor""")
+    parser.add_argument('tensor_path',
+                        nargs='?',
+                        help="By default the standard output is used, "
+                             "but only if something is piped or redirected to it.")
     parser.add_argument('-k', '--k', default=5,
                         help="""The number of printed items. If k <= 0 it prints the whole tensor. Default: 5.""")
     parser.add_argument('-a', '--axis', default=1, help="""The axis for doing topk. Default: 1""")
@@ -60,7 +65,18 @@ def tensor_name(path):
 def main():
     np.set_printoptions(precision=3, suppress=True, floatmode='fixed', threshold=sys.maxsize)
     args = get_args()
-    tensor = read_nnef_tensor(args.tensor_path)
+
+    if args.tensor_path is None:
+        utils.set_stdin_to_binary()
+
+        if sys.stdin.isatty():
+            print("No input provided!", file=sys.stderr)
+            exit(1)
+
+        tensor = nnef.read_tensor(sys.stdin)[0]
+        assert not sys.stdin.read()
+    else:
+        tensor = read_nnef_tensor(args.tensor_path)
 
     print('Shape:', list(tensor.shape))
 
@@ -78,7 +94,7 @@ def main():
         values = np.squeeze(values)
         indices = np.squeeze(indices)
         print("TopK({}, k={}, axis={}):\nValues:\n{}\nIndices:\n{}".format(
-            tensor_name(args.tensor_path), k, axis, values, indices))
+            tensor_name(args.tensor_path) if args.tensor_path is not None else "stdin", k, axis, values, indices))
 
 
 if __name__ == '__main__':
