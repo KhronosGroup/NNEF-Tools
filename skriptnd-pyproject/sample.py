3832759e97c350e3b3ff141e6041dd181a481aee
Viktor Gyenes <viktor.gyenes@aimotive.com>
2025-03-27 18:14:19 -0300

    add more sample code

--- skriptnd-pyproject/sample.py
+++ skriptnd-pyproject/sample.py
@@ -1,8 +1,51 @@
 import skriptnd as nd
 import numpy as np
+import sys
+import os
 
-graph = nd.read_model('alexnet.nds')
-model = nd.compile_model(graph)
-input = np.random.random((1,3,224,224)).astype(np.float32)
-output, = model(input)
-print(output.dtype, output.shape)
+
+BASE_FOLDER = os.path.normpath(os.path.join(os.path.dirname(__file__), 'skriptnd/test/'))
+
+
+def roundtrip_test():
+    print('Reading model...')
+    model = nd.read_model(BASE_FOLDER + '/test.nds')
+    if not model:
+        exit(-1)
+
+    print('Writing model back...')
+    nd.print_model(model, file=sys.stdout)
+    nd.write_model(model, BASE_FOLDER + '/test_round_trip.nds')
+    print('Reading model again...')
+    nd.read_model(BASE_FOLDER + '/test_round_trip.nds')
+
+
+def compile_test():
+    print('Reading model...')
+    model = nd.read_model(BASE_FOLDER + '/test.nds')
+    if not model:
+        exit(-1)
+
+    print("Compiling model '{}'...".format(model.name))
+    compiled = nd.compile_model(model, keep_generated_code=True)
+    print("Compiled model '{}'".format(model.name))
+    print("Inputs:", compiled.input_info())
+    print("Outputs:", compiled.output_info())
+
+
+def execution_test():
+    print('Reading model...')
+    model = nd.read_model(BASE_FOLDER + '/../../alexnet.nds')
+    if not model:
+        exit(-1)
+
+    print("Compiling model '{}'...".format(model.name))
+    compiled = nd.compile_model(model)
+
+    print("Executing model '{}'...".format(model.name))
+    input = np.random.random((1,3,224,224)).astype(np.float32)
+    output, = compiled(input)
+    print("Done", output.dtype, output.shape)
+
+
+roundtrip_test()
