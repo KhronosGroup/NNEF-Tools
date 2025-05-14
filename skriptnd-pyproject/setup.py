1f6944028c6424c0101190465b034db12269b3d5
Viktor Gyenes <viktor.gyenes@aimotive.com>
2025-05-14 16:44:35 +0200

    import fixes in setup.py for SkriptND project

--- skriptnd-pyproject/setup.py
+++ skriptnd-pyproject/setup.py
@@ -1,8 +1,7 @@
 from setuptools import Extension, setup
+import shutil
 import numpy
 import os
-import shutil
-from os import name as os_name
 from sys import platform
 
 
@@ -25,7 +24,7 @@ setup(
             language="c++",
             extra_compile_args=["/std:c++17", "/Zc:preprocessor"] if platform in ["win32", "cygwin"] else
                                ["-std=c++17", "-mmacosx-version-min=10.14"] if platform in ["darwin"] else
-                               ["-std=c++17"] if os_name != "nt" else [],
+                               ["-std=c++17"] if os.name != "nt" else [],
         )
     ],
     package_data={"skriptnd.stdlib": ["*.sknd"]}
